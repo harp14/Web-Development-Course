d84b1c476552385b2a890126c707af7d5e43ae4e
Harpreet Matharu <harp4491@gmail.com>
2021-01-20 18:33:42 +0000

    Add WeatherProject files

---
+++ Section 19 - Express.js with Node.js/WeatherProject/app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const https = require("https");
+const bodyParser = require("body-parser");
+require('dotenv').config();
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res){
+    const query = req.body.cityName;
+    const apiKey = process.env.API_KEY;
+    const unit = "metric";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&units=" + unit + "&appid=" + apiKey;
+    
+    https.get(url, function(response){
+        response.on("data", function(data){
+            const weatherData = JSON.parse(data);
+            const temp = weatherData.main.temp;
+            const weatherDescription = weatherData.weather[0].description;
+            const icon = weatherData.weather[0].icon;
+            const imageURL = "http://openweathermap.org/img/wn/" + icon + "@4x.png"
+            res.write("<p>It is currently " + weatherDescription + "</p>");
+            res.write("<h1>The temperature in " + query + " is: " + temp + " degrees Celsius</h1>");
+            res.write("<img src=" + imageURL + ">");
+            res.send();
+        });
+    });
+});
+
+app.listen(3000, function(){
+    console.log("Server is running on port 3000.");
+});

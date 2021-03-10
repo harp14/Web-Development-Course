362147a03aebd6d3aea22b7d9d8bef8cca4a8f6a
Harpreet Matharu <harp4491@gmail.com>
2021-03-10 14:08:22 +0000

    Add ES6 destructuring exercise

---
+++ Section 33 - React.js/es6-destructuring/src/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import cars from "./car-data";
+
+const [honda, tesla] = cars;
+const {speedStats: {topSpeed: hondaTopSpeed}} = honda;
+const {speedStats: {topSpeed: teslaTopSpeed}} = tesla;
+const {coloursByPopularity: [hondaTopColour]} = honda;
+const {coloursByPopularity: [teslaTopColour]} = tesla;
+
+ReactDOM.render(
+  <table>
+    <thead>
+      <tr>
+        <th>Brand</th>
+        <th>Top Speed</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>{tesla.model}</td>
+        <td>{teslaTopSpeed}</td>
+        <td>{teslaTopColour}</td>
+      </tr>
+      <tr>
+        <td>{honda.model}</td>
+        <td>{hondaTopSpeed}</td>
+        <td>{hondaTopColour}</td>
+      </tr>
+    </tbody>
+  </table>,
+  document.getElementById("root")
+);

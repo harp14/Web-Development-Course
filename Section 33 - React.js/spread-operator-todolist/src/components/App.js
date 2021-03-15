72a3cb1f78437ac1049c345c7faf03991572db3f
Harpreet Matharu <harp4491@gmail.com>
2021-03-15 21:51:10 +0000

    Add React.js todolist app

---
+++ Section 33 - React.js/spread-operator-todolist/src/components/App.js
@@ -0,0 +1,44 @@
+import React, {useState} from "react";
+
+function App() {
+    const [inputText, setInputText] = useState("");
+    const [items, setItems] = useState([]);
+
+    // Add item to items array
+    function addItem() {
+        // Add previously added items to items array then add new item
+        setItems(prevItems => [...prevItems, inputText]);
+        setInputText("");
+    }
+
+    // Update inputText to new entered value
+    function handleChange(event) {
+        const newValue = event.target.value;
+        setInputText(newValue);
+    }
+
+    return (
+        <div className="container">
+            <div className="heading">
+            <h1>To-Do List</h1>
+            </div>
+            <div className="form">
+            <input
+                onChange={handleChange}
+                type="text"
+                value={inputText}
+            />
+            <button onClick={addItem}>
+                <span>Add</span>
+            </button>
+            </div>
+            <div>
+            <ul>
+                {items.map(item => <li>{item}</li>)}
+            </ul>
+            </div>
+        </div>
+    );
+}
+
+export default App;
